095a871e27ab566cd89fc9dcc32aad1c92419824
VlPelov <vpelov@mail.bg>
2022-03-04 14:20:19 +0200

    Add files via upload

---
+++ P02.OddOccurrences.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read input
+
+            string[] inputData = Console.ReadLine()
+                .ToLower()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Dictionary<string, int> dataDictionary = new Dictionary<string, int>();
+
+            foreach (string item in inputData)
+            {
+                if (dataDictionary.ContainsKey(item))
+                {
+                    dataDictionary[item] += 1;
+                }
+                else
+                {
+                dataDictionary.Add(item, 1);                        
+                }
+            }
+
+            // Output data
+            //SortedDictionary<string, int> dataSortDict = new SortedDictionary<string, int>(dataDictionary);
+
+            foreach (var item in dataDictionary)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write($"{item.Key} ");
+                }
+            }
+
+        }
+    }
+}
